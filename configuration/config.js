66ff0e6b24ed4c08a03aa82ffb9420485be1f0a0
rishabh <rishabhsinghal2410@gmail.com>
2017-11-24 12:38:20 +0530

    server js with fb api call

---
+++ configuration/config.js
@@ -0,0 +1,10 @@
+module.exports={
+    "facebook_api_key"      :     "1702849756624256",
+    "facebook_api_secret"   :     "2c221856fe2413114e55c0d27fa3df4c",
+    "callback_url"          :     "http://localhost:8000/auth/facebook/callback",
+    "use_database"          :     "false",
+    "host"                  :     "localhost",
+    "username"              :     "root",
+    "password"              :     "",
+    "database"              :     "DB NAME"
+  }
